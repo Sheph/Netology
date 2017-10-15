e6b30ba669227df07d23e2ffbb0cbab6a8c41732
Stanislav Vorobiov <sheffmail@mail.ru>
2017-10-15 19:33:37 +0300

    task 1: done

--- 1/main.py
+++ 1/main.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
+import pandas as pd
+
 if __name__ == "__main__":
-    pass
+    mountains = pd.read_csv("Mountains.csv")
+    parentMountains = mountains.groupby("Parent mountain").size()
+    print(len(parentMountains[parentMountains > 3]))
